dcf604b3d6cb38a7473bb6c480ac3bfc530bb94e
Amine Dammak <amine.dammak@foodomarket.com>
2024-12-10 16:21:06 +0100

    Signin singout from client component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 import { Button } from '@nextui-org/react';
 import * as actions from '@/actions';
 import { auth } from '@/auth';
+import Profile from '@/components/profile';
 
 export default async function Home() {
   const session = await auth();
@@ -14,7 +15,12 @@ export default async function Home() {
         <Button type="submit">Sign out</Button>
       </form>
 
-      {session?.user ? <div>Signed in</div> : <div>Signed Out</div>}
+      {session?.user ? (
+        <div>Signed in {JSON.stringify(session?.user)}</div>
+      ) : (
+        <div>Signed Out</div>
+      )}
+      <Profile />
     </div>
   );
 }
