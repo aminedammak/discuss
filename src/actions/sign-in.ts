0454ca8780b291480ec06759cee1718e0e56a51c
Amine Dammak <amine.dammak@foodomarket.com>
2024-12-10 17:40:32 +0100

    Add comments on what paths need to be revalidated for each server action

---
+++ src/actions/sign-in.ts
@@ -0,0 +1,9 @@
+'use server';
+
+import * as auth from '@/auth';
+
+//It's optional to define those sign in and out actions, but it's recommanded to do that
+export async function signIn() {
+  //In this case we have only github as a auth provider
+  return auth.signIn('github');
+}
