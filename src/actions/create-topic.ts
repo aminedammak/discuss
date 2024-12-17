a8606c106c3a04af4bc62cd2aacd0babb40f4ca2
amine.dammak <amine.dammak@foodomarket.com>
2024-12-17 12:00:39 +0100

    Topic creation

--- src/actions/create-topic.ts
+++ src/actions/create-topic.ts
@@ -2,6 +2,11 @@
 
 import { z } from "zod";
 import { auth } from "@/auth";
+import type { Topic } from "@prisma/client";
+import { revalidatePath } from "next/cache";
+import { redirect } from "next/navigation";
+import { db } from "@/db";
+import paths from "@/paths";
 
 const createTopicSchema = z.object({
   name: z
@@ -48,9 +53,30 @@ export async function createTopic(
     };
   }
 
-  return {
-    errors: {},
-  };
+  let topic;
 
-  //TODO: revalidate the homepage
+  try {
+    topic = await db.topic.create({
+      data: {
+        slug: result.data.name,
+        description: result.data.description,
+      },
+    });
+  } catch (err: unknown) {
+    if (err instanceof Error) {
+      return {
+        errors: {
+          _form: [err.message],
+        },
+      };
+    } else {
+      return {
+        errors: {
+          _form: ["Something went wrong"],
+        },
+      };
+    }
+  }
+  revalidatePath("/");
+  redirect(paths.topicShow(topic?.slug));
 }
